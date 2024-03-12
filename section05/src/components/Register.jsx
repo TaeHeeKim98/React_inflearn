55164443d6b566dd1bd39919a76e8b11e0455269
TaeheeKim <eksrnsdhkdwk@gmail.com>
2024-03-12 16:55:02 +0900

    no message

---
+++ section05/src/components/Register.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+//간단한 회원가입 폼
+const Register = () => {
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+
+  const onChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
+      </div>
+      <div>
+        <input
+          name="birth"
+          value={input.birth}
+          type="date"
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <select name="country" value={input.country} onChange={onChange}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+      </div>
+      <div>
+        <textarea name="bio" value={input.bio} onChange={onChange}></textarea>
+      </div>
+      <div>이름 : {input.name}</div>
+      <div>생일 : {input.birth}</div>
+      <div>국적 : {input.country}</div>
+      <div>자기소개 : {input.bio}</div>
+    </>
+  );
+};
+export default Register;
