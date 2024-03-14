53b8b8be905047def38925e98e8a7d9b73dfda0d
TaeheeKim <eksrnsdhkdwk@gmail.com>
2024-03-14 09:16:04 +0900

    no message

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import Register from "./components/Register";
-import HoolExam from "./components/HookExam";
+import HookExam from "./components/HookExam";
 
 function App() {
   return (
     <>
-      <HoolExam />
+      <HookExam />
     </>
   );
 }
